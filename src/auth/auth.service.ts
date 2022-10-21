4090319072d9af50cf787a03958621c37528cd71
Guilherme Ramos <guioliveiraramos10@gmail.com>
2022-10-21 14:44:03 -0300

    feat: finish create user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signup({ email, password }: AuthDto) {
-    const hash = await argon.hash(password);
-    const user = await this.prisma.user.create({
-      data: { email, hash },
-    });
+    try {
+      const hash = await argon.hash(password);
+      const user = await this.prisma.user.create({
+        data: { email, hash },
+      });
 
-    delete user.hash;
+      delete user.hash;
 
-    return user;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+
+      throw error;
+    }
   }
 
   signin() {
