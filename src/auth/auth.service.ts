58ed3c3510c8081d50d76e84db76a79c96eacebc
Guilherme Ramos <guioliveiraramos10@gmail.com>
2022-10-21 14:34:19 -0300

    feat: creating user and saving on database

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signin() {
-    return { msg: 'signin' };
+  async signup({ email, password }: AuthDto) {
+    const hash = await argon.hash(password);
+    const user = await this.prisma.user.create({
+      data: { email, hash },
+    });
+
+    delete user.hash;
+
+    return user;
   }
 
-  signup() {
+  signin() {
     return { msg: 'signup' };
   }
 }
